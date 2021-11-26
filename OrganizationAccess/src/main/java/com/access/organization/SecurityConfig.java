e5c17fc8a5ff1638bc64e1ba5ee206d1edd4fd2a
MythiliPrabaharan <mythili.prabaharan13@gmail.com>
2021-11-26 09:07:17 +0530

    Update SecurityConfig.java

--- OrganizationAccess/src/main/java/com/access/organization/SecurityConfig.java
+++ OrganizationAccess/src/main/java/com/access/organization/SecurityConfig.java
@@ -4,24 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+	@Autowired
+	private AuthenticationEntryPoint authEntryPoint;
+
 	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception
+	protected void configure(HttpSecurity httpSecurity) throws Exception 
 	{
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/**").permitAll();
-		httpSecurity.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
-		//httpSecurity.csrf().disable();
+		httpSecurity.csrf().disable().authorizeRequests()
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.authenticationEntryPoint(authEntryPoint);
+		
 		httpSecurity.headers().frameOptions().disable();
+		
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
 	{
-		auth.inMemoryAuthentication().withUser("adminuser").password("{noop}adminpassword").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("adminuser").password("adminpassword").roles("USER");
 	}
 }
